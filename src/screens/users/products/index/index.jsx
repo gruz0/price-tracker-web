5bbf6441f4b557de2fa23b6072991d6fb20336f7
Alexander Kadyrov <gruz0.mail@gmail.com>
2022-05-28 13:48:49 +0300

    Add short brief

--- src/screens/users/products/index/index.jsx
+++ src/screens/users/products/index/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useContext } from 'react'
+import Link from 'next/link'
 
-import { Divider, Message } from 'semantic-ui-react'
+import { Segment, Divider, Message, List } from 'semantic-ui-react'
 
 import Products from '../../../../components/Products'
 import ErrorWrapper from '../../../../components/ErrorWrapper'
@@ -11,7 +12,7 @@ import useProducts from '../../../../hooks/useProducts'
 import DisplayContext from '../../../../context/display-context'
 
 const Screen = () => {
-  const { token, logout } = useAuth()
+  const { user, token, logout } = useAuth()
   const { data, isLoading, error } = useProducts(token)
   const { isSmallScreen } = useContext(DisplayContext)
 
@@ -20,6 +21,10 @@ const Screen = () => {
       return logout()
     }
   }, [error])
+
+  if (!user) {
+    return <JustOneSecond />
+  }
 
   return (
     <>
@@ -31,6 +36,94 @@ const Screen = () => {
             <JustOneSecond />
           ) : (
             <>
+              <Message>
+                <Message.Header>
+                  Рады вас видеть и хотим поблагодарить, что согласились помочь
+                  нам в тестировании!
+                </Message.Header>
+
+                <Segment>
+                  <p>
+                    <strong>Что мы хотим протестировать:</strong>
+                  </p>
+
+                  <List ordered>
+                    <List.Item>Удобство использования системой</List.Item>
+                    <List.Item>
+                      Найти проблемы и ошибки при обработке цен и наличия
+                      товаров
+                    </List.Item>
+                    <List.Item>Выяснить интерес пользователей</List.Item>
+                  </List>
+                </Segment>
+
+                <Segment>
+                  <p>
+                    <strong>На сегодня план очень простой и понятный:</strong>
+                  </p>
+
+                  <List bulleted>
+                    <List.Item>
+                      В форму ниже добавьте по очереди несколько ссылок на свои
+                      товары из Озона и/или Вайлдберрис
+                    </List.Item>
+
+                    <List.Item>
+                      Если пользуетесь только одним магазином, то значит ссылки
+                      только из него. Если не знаете как -{' '}
+                      <Link href="/help" passHref>
+                        <a>мы подготовили инструкции</a>
+                      </Link>
+                    </List.Item>
+
+                    <List.Item>
+                      Через полчаса товары появятся в системе и мы начнём
+                      собирать цены и проверять наличие
+                    </List.Item>
+
+                    <List.Item>
+                      Перейдите по{' '}
+                      <a
+                        href={`https://t.me/chartik_ru_bot?start=${user.id}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        title="Наш бот в Telegram"
+                      >
+                        этой ссылке{' '}
+                      </a>
+                      в Телеграм бота, чтобы мы добавили аккаунт в систему
+                    </List.Item>
+
+                    <List.Item>
+                      Ответьте на вопросы{' '}
+                      <a
+                        href="https://forms.gle/Vojfs8t1hNAhKc5q8"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        простой формы
+                      </a>
+                    </List.Item>
+                  </List>
+                </Segment>
+
+                <Segment>
+                  <p>
+                    <strong>Что дальше?</strong>
+                  </p>
+
+                  <List bulleted>
+                    <List.Item>
+                      Завтра к вечеру мы будем знать 2-3 цены по каждой позиции
+                    </List.Item>
+                    <List.Item>
+                      Попробуйте пока полазить в системе и понять, удобно ли вам
+                      этим вообще пользоваться или же всё слишком сложно
+                    </List.Item>
+                  </List>
+                </Segment>
+              </Message>
+
               <AddNewProduct token={token} isSmallScreen={isSmallScreen} />
 
               {!isSmallScreen && <Divider hidden />}
