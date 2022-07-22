ff048b54b01b69aadb61842dfe568e93c4b121dd
Alexander Kadyrov <gruz0.mail@gmail.com>
2022-07-22 22:32:00 +0300

    Change product status to hold when there are no users

---
+++ src/services/products_service.js
@@ -0,0 +1,18 @@
+import { validateProductId } from '../lib/validators'
+import { ProductRepository as repo } from '../repositories/product_repository'
+
+export const ProductsService = {
+  isOwnedByUsers: async (productId) => {
+    validateProductId(productId)
+
+    const ownedByUsers = await repo.ownedByUsers(productId)
+
+    return ownedByUsers.length > 0
+  },
+
+  moveToHold: async (productId) => {
+    validateProductId(productId)
+
+    return await repo.changeStatusToHold(productId)
+  },
+}
